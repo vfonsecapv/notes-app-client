84808abee3856bc7b6e949ab0cdcf3609619bf6b
victor_fonseca <victor.fonseca@sciensa.com>
2017-05-22 11:49:19 -0300

    Change config.js file

--- src/config.js
+++ src/config.js
@@ -2,20 +2,20 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     //YOUR AWS S3 BUCKET NAME
-    BUCKET: 'notes-app-uploads-vf'
+    BUCKET: ''
   },
   apiGateway: {
     //EG.: https://lambdaname.execute-api.region.amazonaws.com/dev
-    URL: 'https://bo09rj6y1d.execute-api.us-east-1.amazonaws.com/prod/',
+    URL: '',
   },
   cognito: {
     //EG.: region-code
-    USER_POOL_ID : 'us-east-1_5jI2CpVD4',
+    USER_POOL_ID : '',
     //COGNITO APP CLIENT ID
-    APP_CLIENT_ID : '7c4jdqu5ddttadht1dtpnn3b8j',
+    APP_CLIENT_ID : '',
     //AWS REGION us-east-1 us-east-2 or any other
-    REGION: 'us-east-1',
+    REGION: '',
     //COGNITO FEDERATE IDENTIFY POOL
-    IDENTITY_POOL_ID: 'us-east-1:c2202ad6-5d3c-446a-9614-06dadeec102f',
+    IDENTITY_POOL_ID: '',
   }
 };
